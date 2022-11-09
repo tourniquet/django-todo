efb9177edd701576ab0937c1c2406e3172f3be9a
tourniquet <admyn3d@gmail.com>
2022-11-09 16:37:14 +0000

    created data model

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from todo_app.models import ToDoItem, ToDoList
+
+admin.site.register(ToDoItem)
+admin.site.register(ToDoList)
